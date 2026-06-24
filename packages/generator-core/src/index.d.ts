25f89a6126a8be135766fc7b702175960cc323aa
SlothMD <sloth.dc@gmail.com>
2026-06-24 19:49:35 -0400

    Ignore generated TypeScript artifacts

--- packages/generator-core/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { GenerationConfig, PrimaryWorld, SelectedValues, WorldMetrics, WorldProject, createDefaultConfig, defaultParameterRanges } from '@world-forge/shared';
-import { SeededRandom } from './random';
-export { SeededRandom, createDefaultConfig, defaultParameterRanges };
-export declare function generateProject(input?: Partial<GenerationConfig>): WorldProject;
-export declare function calculateMetrics(world: PrimaryWorld, values: SelectedValues): WorldMetrics;
-//# sourceMappingURL=index.d.ts.map
